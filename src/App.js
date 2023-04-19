d5fffc6bbc63c1822fb9b924513487d0297af670
satishhalhalli <satishhalhalli7484@gmail.com>
2023-04-19 22:25:40 +0530

    added auth and slices

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import Header from './components/Header';
+import Counter from './components/Counter';
+
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+function App() {
+
+  const isAuth=useSelector(state => state.auth.isAuthanticated);
+  return (
+    <Fragment>
+      <Header/>
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile/>}
+      <Counter />
+     
+    </Fragment>
+    
+  );
+}
+
+export default App;
